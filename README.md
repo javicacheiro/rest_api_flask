deccae504c0415e539ece33015d0f2e94313e7bc
Javier Cacheiro <jlopez@cesga.es>
2016-02-02 19:58:11 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+Flash REST API template
+=======================
+
+This repository contains a template for building REST APIs based on Flask.
+
+It is based on layout recommended by Miguel Grinberg on the 
+[git repo](https://github.com/miguelgrinberg/oreilly-flask-apis-video.git)
+of the [Building Web APIs with Flask](http://bit.ly/flaskapi) course.
+
+```
+git clone https://github.com/javicacheiro/rest_api_flask
+cd rest_api_flask
+virtualenv venv
+pip install -r requirements.txt
+```
+
+To add new REST resources create new modules inside rest/app/api_v1 
+and add them to \_\_init\_\_.py:
+
+    from . import products, newresource
---
+++ rest/app/models.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from dateutil import parser as datetime_parser
+from dateutil.tz import tzutc
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import url_for, current_app
+from . import db
+from .exceptions import ValidationError
+from .utils import split_url
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expires_in=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expires_in)
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    items = db.relationship('Item', backref='product', lazy='dynamic')
+
+    def get_url(self):
+        return url_for('api.get_product', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'name': self.name
+        }
+
+    def import_data(self, data):
+        try:
+            self.name = data['name']
+        except KeyError as e:
+            raise ValidationError('Invalid product: missing ' + e.args[0])
+        return self
